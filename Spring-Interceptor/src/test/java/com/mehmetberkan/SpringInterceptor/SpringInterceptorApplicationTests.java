16d0fdd9f0cb7ab8e8dd0b064ff12decbb979e8c
Mehmet Berkan <mehmetberkangorel@gmail.com>
2023-09-25 20:42:28 +0300

    Initial commit

---
+++ Spring-Interceptor/src/test/java/com/mehmetberkan/SpringInterceptor/SpringInterceptorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mehmetberkan.SpringInterceptor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringInterceptorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
